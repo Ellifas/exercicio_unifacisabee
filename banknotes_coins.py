0eaab8e43717749b05c6f915da24fc5644ddc9c1
Ellifas <ellifasr@outlook.com>
2022-09-04 17:11:54 -0300

    exec

---
+++ banknotes_coins.py
@@ -0,0 +1,78 @@
+# value = float(input())
+# cem = cinquenta = vinte = dez = cinco = dois = um = 0
+# cincents = vintecincents = dezcents = cincocents = cents = 0
+#
+# value = float("%.2f" % value)
+# if int(value/100) >= 1:
+#     cem = int(value/100)
+#     value -= cem*100
+#
+# value = float("%.2f" % value)
+# if int(value/50) >= 1:
+#     cinquenta = int(value/50)
+#     value -= cinquenta*50
+#
+# value = float("%.2f" % value)
+# if int(value/20) >= 1:
+#     vinte = int(value/20)
+#     value -= vinte*20
+#
+# value = float("%.2f" % value)
+# if int(value/10) >= 1:
+#     dez = int(value/10)
+#     value -= dez*10
+#
+# value = float("%.2f" % value)
+# if int(value/5) >= 1:
+#     cinco = int(value/5)
+#     value -= cinco*5
+#
+# value = float("%.2f" % value)
+# if int(value/2) >= 1:
+#     dois = int(value/2)
+#     value -= dois*2
+#
+# value = float("%.2f" % value)
+# if int(value/1) >= 1:
+#     um = int(value/1)
+#     value -= um*1
+#
+# value = float("%.2f" % value)
+# if int(value/0.50) >= 1:
+#     cincents = int(value/0.50)
+#     value -= cincents*0.50
+#
+# value = float("%.2f" % value)
+# if int(value/0.25) > 1:
+#     vintecincents = int(value/0.25)
+#     value -= vintecincents*0.25
+#
+# value = float("%.2f" % value)
+# if int(value/0.10) >= 1:
+#     dezcents = int(value/0.10)
+#     value -= dezcents*0.10
+#
+# value = float("%.2f" % value)
+# if int(value/0.05) >= 1:
+#     cincocents = int(value/0.05)
+#     value -= cincocents*0.05
+#
+# value = float("%.2f" % value)
+# if int(value/0.01) >= 0.998:
+#     cents = int(value/0.01)
+#     value -= cents*0.01
+#
+# print("NOTAS:")
+# print(cem, "nota(s) de R$ 100.00")
+# print(cinquenta, "nota(s) de R$ 50.00")
+# print(vinte, "nota(s) de R$ 20.00")
+# print(dez, "nota(s) de R$ 10.00")
+# print(cinco, "nota(S) de R$ 5.00")
+# print(dois, "nota(s) de R$ 2.00")
+# print("MOEDAS:")
+# print(um, "moeda(s) de R$ 1.00")
+# print(cincents, "moeda(s) de R$ 0.50")
+# print(vintecincents, "moeda(s) de R$ 0.25")
+# print(dezcents, "moeda(s) de R$ 0.10")
+# print(cincocents, "moeda(s) de R$ 0.05")
+# print(cents, "moeda(s) de R$ 0.01")
---
+++ selection_test.py
@@ -0,0 +1,10 @@
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+E = (C + D)
+
+if B > C and D > A:
+    if C + D > A + B:
+        if C and D /2
+            print("Valores aceitos")
